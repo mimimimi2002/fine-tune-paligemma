db33eb4629469e727436d634f13f872543012065
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-07-28 21:38:32 +0900

    feat: modify config; dataset id and lower batch size

--- configs/object_detection_config.py
+++ configs/object_detection_config.py
@@ -1,8 +1,8 @@
 import torch
 
-DATASET_ID = "ariG23498/license-detection-paligemma"
+DATASET_ID = "mimimimi2002/license-detection-paligemma"
 MODEL_ID = "google/paligemma-3b-pt-224"
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 LEARNING_RATE = 5e-5
 MODEL_DTYPE = torch.bfloat16
 MODEL_REVISION = "bfloat16"
