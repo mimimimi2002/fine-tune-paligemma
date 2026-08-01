3874232417a69c4af01be42a128debcec3de80d5
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-08-01 16:58:55 +0900

    fix: add plate number to label in ocr dataset

--- object_detection/create_od_ocr_dataset.py
+++ object_detection/create_od_ocr_dataset.py
@@ -4,6 +4,22 @@ import numpy as np
 
 ocr = PaddleOCR(use_angle_cls=True, lang='en')  # Initialize PaddleOCR
 
+def read_plate_number(image, bbox):
+    x1, y1, x2, y2 = bbox
+    cropped_image = image.crop((int(x1), int(y1), int(x2), int(y2)))
+    ocr_results = ocr.ocr(np.array(cropped_image))
+
+    # Extract text from OCR results
+    plate_number = ""
+    if ocr_results is not None:
+        for line in ocr_results:
+            if line is not None:  # Check if the line has text
+                for word_info in line:
+                    text = word_info[1][0]  # The recognized text
+                    plate_number += text + " "
+
+    return plate_number.strip()  # Return the plate number without trailing spaces
+
 def coco_to_xyxy(coco_bbox):
     x, y, width, height = coco_bbox
     x1, y1 = x, y
@@ -27,19 +43,14 @@ def convert_to_detection_string(bboxs, image_width, image_height, image=None):
         ]
         detection_string = "".join(locs) + f" {name}"
         detection_strings.append(detection_string)
-        
+
+        name = "plate"
         if image is not None:
-            cropped_image = image.crop((int(x1), int(y1), int(x2), int(y2)))
-            ocr_results = ocr.ocr(np.array(cropped_image))
-            # Extract text from OCR results
-            ocr_texts = []
-            if ocr_results is not None:
-                for line in ocr_results:
-                    if line is not None:  # Check if the line has text
-                        for word_info in line:
-                            text = word_info[1][0]  # The recognized text
-                            ocr_texts.append(text)
-            detection_strings.append(" ".join(ocr_texts))
+            plate_number = read_plate_number(image, (x1, y1, x2, y2))
+            if plate_number:
+                name = f"plate {plate_number}"   # 例: "plate ABC1234"
+
+        detection_strings.append("".join(locs) + f" {name}")
 
     return " ; ".join(detection_strings)
 
@@ -57,7 +68,7 @@ def format_objects_with_ocr(example):
     bboxs = example["objects"]["bbox"]
     formatted_objects = convert_to_detection_string(bboxs, width, height, example['image'])
     print(f"[INFO] formatted objects: {formatted_objects}")
-    
+
     return {"label_for_paligemma": formatted_objects}
 
 
@@ -73,4 +84,4 @@ if __name__ == "__main__":
     dataset["test"] = dataset["test"].map(format_objects_with_ocr)
 
     # push to hub
-    dataset.push_to_hub("license-detection-paligemma")
+    dataset.push_to_hub("license-detection-paligemma-ocr")
